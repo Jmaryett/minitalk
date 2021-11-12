ba47f590bc2d9a129f6527b41c10b2c617e61910
Jmaryett <wtfmahariel@gmail.com>
2021-11-12 20:43:44 +0300

    meh

--- client.c
+++ client.c
@@ -25,7 +25,7 @@ void	sending_bits(char c, int pid)
 			i /= 2;
 			g_recieve = 0;
 		}
-		//usleep(1000);
+		//usleep(600);
 	}
 }
 
@@ -58,15 +58,21 @@ int	main(int argc, char **argv)
  	struct sigaction	sigac;
 
 	g_recieve = 1;
+	sigemptyset(&sigac.sa_mask);
+	sigaddset(&sigac.sa_mask, SIGINT);
+	sigaddset(&sigac.sa_mask, SIGQUIT);
+	sigaddset(&sigac.sa_mask, SIGUSR1);
   	sigac.sa_flags = SA_SIGINFO;
 	sigac.sa_sigaction = cl_handler;
 	if (sigaction(SIGUSR2, &sigac, NULL) == -1)
 		errors("Error in client sigaction\n");
+	if (ft_atoi(argv[1]) < 0)
+		errors("Wrong PID!\n");
 	if (argc == 3)
 		send_str(ft_atoi(argv[1]), argv[2]);
 	else
 		errors("Wrong arguments!\n");
- 	/* while (1)
-		pause (); */
+	while (1)
+		pause ();
 	return (0);
 }
